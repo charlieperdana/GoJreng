eb5f2ff955698f2038ad484a90e240fad8abd511
charlieperdana <charlie.siagian@gmail.com>
2021-06-04 12:18:32 +0700

    Initial Commit

---
+++ GoJreng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoJreng
+//
+//  Created by charlie siagian on 04/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
