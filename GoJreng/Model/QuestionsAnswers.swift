cea599d3a61ecca28466f47fcdf969d17d92741d
Novi Gunawan <novgunawan07@gmail.com>
2021-06-09 14:33:49 +0700

    Add Model for QuestionsAnswers and Stages

---
+++ GoJreng/Model/QuestionsAnswers.swift
@@ -0,0 +1,19 @@
+//
+//  Questions.swift
+//  GoJreng
+//
+//  Created by Novi Gunawan on 08/06/21.
+//
+
+import Foundation
+import UIKit
+
+struct Questions {
+    var questionSound: String
+    var answer: [Answers]
+}
+
+struct Answers {
+    var answerSound: String
+    var isCorrect: Bool
+}
