120dbdea236a3f806a990b09be8c343b1df5c6a5
angelicajan <angelicajanissia@outlook.com>
2021-06-19 18:45:30 +0700

    create home helper and delete unused code

--- GoJreng/HomePage/HomeHelper.swift
+++ GoJreng/HomePage/HomeHelper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class HomeHelper{
+
+  static func checkHSinited(){
+    let defaults = UserDefaults.standard
+    let keyArr = ["initChecked", "hS1", "hS2", "hS3"]
+
+    if (defaults.value(forKey: "initChecked") != nil) != true{ //returns true if initChecked == nil
+        print("not checked")
+        defaults.setValue(true, forKey: "initChecked")
+        defaults.setValue(0, forKey: "hS1")
+        defaults.setValue(0, forKey: "hS2")
+        defaults.setValue(0, forKey: "hS3")
+    }
+    else {
+        print("defaults set:")
+        let dictionary = defaults.dictionaryRepresentation()
+            dictionary.keys.forEach { key in
+                if keyArr.contains(key){
+                    print(key)
+                    print(defaults.value(forKey: key)!)
+                    print("============")
+                }
+            }
+    }
+  }
+  
+}
--- GoJreng/HomePage/HomePageViewController.swift
+++ GoJreng/HomePage/HomePageViewController.swift
@@ -8,38 +8,7 @@
 
 import UIKit
 
-func resetDefaults() {
-    let defaults = UserDefaults.standard
-    let dictionary = defaults.dictionaryRepresentation()
-    dictionary.keys.forEach { key in
-        defaults.removeObject(forKey: key)
-    }
-}
 
-func checkHSinited(){
-    let defaults = UserDefaults.standard
-    let keyArr = ["initChecked", "hS1", "hS2", "hS3"]
-    
-    if (defaults.value(forKey: "initChecked") != nil) != true{ //returns true if initChecked == nil
-        print("not checked")
-        defaults.setValue(true, forKey: "initChecked")
-        defaults.setValue(0, forKey: "hS1")
-        defaults.setValue(0, forKey: "hS2")
-        defaults.setValue(0, forKey: "hS3")
-    }
-    else {
-        print("defaults set:")
-        let dictionary = defaults.dictionaryRepresentation()
-            dictionary.keys.forEach { key in
-                if keyArr.contains(key){
-                    print(key)
-                    print(defaults.value(forKey: key)!)
-                    print("============")
-                }
-            }
-        
-    }
-}
 class HomePageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var bgImage: UIImageView!
@@ -60,59 +29,53 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
         Stages(stageName: "Chord Identification", stageState: .locked, question: [], questionNumber: 10, highScore: 0),
         Stages(stageName: "Chord Progression Identification", stageState: .locked, question: [], questionNumber: 10, highScore: 200)
     ]
-    
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkHSinited()
-//        resetDefaults()
+        HomeHelper.checkHSinited()
 
         stageCollection.dataSource = self
         stageCollection.delegate = self
         
         stageCollection.register(UnlockCollectionViewCell.nib(), forCellWithReuseIdentifier: UnlockCollectionViewCell.identifier)
         stageCollection.register(LockCollectionViewCell.nib(), forCellWithReuseIdentifier: LockCollectionViewCell.identifier)
-
-        let hScore1 = defaults.integer(forKey: "hS1")
-        print("\(hScore1)")
         
-       // print("Ini Hs: \(defaults.value(forKey: "hS1"))")
-        
-//        if Int(defaults.value(forKey: "hS1").intValue as! String) ?? 0 >= 1000  {
+        //unlock stage 2
         if defaults.value(forKey: "hS1") as! Int >= 1400  {
           stage[1].stageState = .unlocked
-        } else{
-            defaults.setValue(0, forKey: "hS1")
         }
-        
-//        if (defaults.value(forKey: "hS2") ?? 0) as! String != nil && Int((defaults.value(forKey: "hS2") ?? 0) as! String) ?? 0 == 1000 {
-//           stage[2].stageState = .unlocked
-//        }
+
+        //unlock stage 3
+        //        if (defaults.value(forKey: "hS2") ?? 0) as! String != nil && Int((defaults.value(forKey: "hS2") ?? 0) as! String) ?? 0 == 1000 {
+        //           stage[2].stageState = .unlocked
+        //        }
 
         
         stageCollection.reloadData()
     }
+
+
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+
         if indexPath.row == 0{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UnlockCollectionViewCell.identifier, for: indexPath) as! UnlockCollectionViewCell
             
             cell.titleLabel.text = stage[indexPath.row].stageName
-            cell.scoreLabel.text = "\(defaults.value(forKey: "hS1") ?? 0)"
+            cell.scoreLabel.text = "\(defaults.integer(forKey: "hS1"))"
             
-//            if Int(defaults.value(forKey: "hS1") as! String) ?? 0 >= 2400{
+
             if defaults.value(forKey: "hS1") as! Int >= 2400{
                 cell.badgeImage.image = #imageLiteral(resourceName: "goldBadge")
-//            } else if (Int(defaults.value(forKey: "hS1") as! String) ?? 0 >= 1500){
             } else if (defaults.value(forKey: "hS1") as! Int >= 1500){
                 cell.badgeImage.image = #imageLiteral(resourceName: "silverBadge")
             }
-            
-            cell.layer.cornerRadius = 14
             
             return cell
         }
@@ -120,18 +83,16 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
         if indexPath.row == 1 {
             if stage[indexPath.row].stageState == StageState.locked{
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LockCollectionViewCell.identifier, for: indexPath) as! LockCollectionViewCell
-                
-                cell.layer.cornerRadius = 14
+
                 cell.bgImage.image = stage[indexPath.row].stageState?.getBackground()
                 cell.lockImage.image = stage[indexPath.row].stageState?.getIcon()
                 cell.titleLabel.text = stage[indexPath.row].stageName
-                cell.isSelected = false
+                cell.isUserInteractionEnabled = false
                 return cell
             }
             
             else{
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UnlockCollectionViewCell.identifier, for: indexPath) as! UnlockCollectionViewCell
-                cell.layer.cornerRadius = 14
                 cell.titleLabel.text = stage[indexPath.row].stageName
                 cell.scoreLabel.text = "\(defaults.value(forKey: "hS2") ?? 0)"
                 if cell.scoreLabel.text == "1000"{
@@ -139,8 +100,6 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
                 } else if cell.scoreLabel.text == "900" || cell.scoreLabel.text == "800"{
                     cell.badgeImage.image = #imageLiteral(resourceName: "silverBadge")
                 }
-                
-                cell.isSelected = true
                 
                 return cell
             }
@@ -150,8 +109,7 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
         if indexPath.row == 2{
             if stage[indexPath.row].stageState == StageState.locked{
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LockCollectionViewCell.identifier, for: indexPath) as! LockCollectionViewCell
-                
-                cell.layer.cornerRadius = 14
+
                 cell.bgImage.image = stage[indexPath.row].stageState?.getBackground()
                 cell.lockImage.image = stage[indexPath.row].stageState?.getIcon()
                 cell.titleLabel.text = stage[indexPath.row].stageName
@@ -159,8 +117,7 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
             }
             else{
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UnlockCollectionViewCell.identifier, for: indexPath) as! UnlockCollectionViewCell
-                
-                cell.layer.cornerRadius = 14
+
                 cell.titleLabel.text = stage[indexPath.row].stageName
                 cell.scoreLabel.text = "\(stage[indexPath.row].highScore ?? 0)"
                 return cell
@@ -181,7 +138,7 @@ class HomePageViewController: UIViewController, UICollectionViewDataSource, UICo
             //showStage2()
             PageHelper.showStage2(currentStoryBoard: self)
         } else {
-          PageHelper.showStage3()
+            PageHelper.showStage3()
         }
     }
 
